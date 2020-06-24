d7386ee15c260784c876279b4994c94ac51faa2b
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-06-24 22:55:00 +0100

    login page form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,7 +3,26 @@ import React from "react";
 const Login = () => {
 
     return(
-        <h1> Login Page</h1>
-    )
+        <div>
+            <div className="container-fluid jumbotron text-center">
+                <h1 className="h1"> Login Page</h1>
+            </div>
+            <form className="container-fluid">
+                <div className="form-group">
+                    <label htmlFor="email">Email address</label>
+                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp"
+                           placeholder="Enter email"/>
+                </div>
+
+
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className="form-control" id="password" placeholder="Password"/>
+                </div>
+
+                <button type="submit" className="btn btn-primary">Login</button>
+            </form>
+        </div>
+)
 }
 export default Login
