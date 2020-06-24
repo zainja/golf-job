ff18540684c527a770aab0f17be6cf5d541f6e34
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-06-24 23:07:19 +0100

    login page form logic

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,23 +1,48 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 const Login = () => {
-
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const handleChange = (e) => {
+        const {id, value} = e.target
+        if (id === "email"){
+            setEmail(value)
+        }else if (id === "password"){
+            setPassword(value)
+        }
+    }
+    const onSubmit = (e) => {
+        e.preventDefault()
+        alert("form submitted")
+    }
     return(
         <div>
             <div className="container-fluid jumbotron text-center">
                 <h1 className="h1"> Login Page</h1>
             </div>
-            <form className="container-fluid">
+
+            <form className="container-fluid" onSubmit={onSubmit}>
                 <div className="form-group">
                     <label htmlFor="email">Email address</label>
-                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp"
+                    <input type="email"
+                           className="form-control"
+                           id="email"
+                           value={email}
+                           onChange={handleChange}
+                           aria-describedby="emailHelp"
                            placeholder="Enter email"/>
                 </div>
 
-
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input type="password" className="form-control" id="password" placeholder="Password"/>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={handleChange}
+                        className="form-control"
+                        id="password"
+                        placeholder="Password"
+                    />
                 </div>
 
                 <button type="submit" className="btn btn-primary">Login</button>
