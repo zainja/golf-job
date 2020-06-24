51689977a1ee690260138a7511370d89711b6d28
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-06-24 22:27:53 +0100

    bootstrap support

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import ReactDOM from 'react-dom'
 import App from './App'
 ReactDOM.render(<App/>, document.getElementById('root'))
