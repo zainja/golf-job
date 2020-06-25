5285f50bedada5627317a497a0c541783f721ad8
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-06-25 08:07:47 +0100

    login page

--- server/query/login.js
+++ server/query/login.js
@@ -1,8 +1,9 @@
 const connection = require('../connection')
 
-module.exports.register = (email, password) => {
+module.exports.login = (email, password) => {
     return new Promise((resolve, reject) => {
-        connection.query("SELECT email FROM users WHERE email = ? AND password = ?", [email, password],(err, result) => {
+        connection.query("SELECT email, password FROM users WHERE email = ? AND password = ?",
+            [email, password],(err, result) => {
             if (err) reject(err)
             if (result.length === 0){
                 reject({message: "incorrect email or password"})
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcrypt')
-
+const register = require("../query/register")
+const login = require('../query/login')
 router.post('/register', async (req, res) => {
-    let {email, firstName, lastName, password} = req.body
+    let {email, firstName, lastName, password, phoneNumber} = req.body
     try{
         const hashedPassword = await hashPassword(password, 10)
-
+        await register.register(email, firstName, lastName, hashedPassword, phoneNumber)
     }catch (e) {
+        res.status(401)
+        res.send({"error": e})
+    }
+})
+
+router.post('/login', async (req, res) => {
+    let {email, password} = req.body
+    try {
+        const result = await login.login(email, password)
+        if ( await comparePasswords(password, result[0].password)){
+            res.status(200)
+            res.send({"message": "login worked"})
+        }
+    }catch (e) {
+        res.status(400)
         res.send({"error": e})
     }
 })
@@ -18,6 +34,15 @@ const hashPassword = (password, salt) => {
         bcrypt.hash(password, salt, (err, hash) => {
             if (err) reject(err)
             resolve(hash)
+        })
+    })
+}
+
+const comparePasswords = (password, hash) => {
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(password, hash, (err, result) => {
+            if(err) reject(err)
+            resolve(result)
         })
     })
 }
