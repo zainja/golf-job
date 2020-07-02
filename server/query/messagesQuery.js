211b0fe2b71cac593779610b99b561cbc86f9f69
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-07-02 16:17:08 +0100

    message queries

--- server/query/messagesQuery.js
+++ server/query/messagesQuery.js
@@ -1,7 +1,23 @@
 const connection = require('../connection')
 
-module.exports.sendMessage = (subject, sender, receiver, message) => {
+module.exports.sendMessage = (sender, receiver, message) => {
     return new Promise((resolve, reject) => {
-        connection.query("INSERT INTO conversations (sender, receiver, message) VALUES (?, ?, ?)")
+        connection.query("INSERT INTO conversations (sender, receiver, message) VALUES (?, ?, ?)",
+            [sender, receiver, message],(err, result) => {
+                if (err) reject(err)
+                resolve(result)
+            })
+    })
+}
+
+module.exports.receiveAllMessages = (user, trainer) => {
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT * FROM conversations
+                                WHERE sender = ? AND receiver = ? 
+                                OR sender = ? AND receiver = ?`,[user, trainer, trainer, user]
+        ,(err, result) => {
+                if (err) reject(err)
+                resolve(result)
+            })
     })
 }
