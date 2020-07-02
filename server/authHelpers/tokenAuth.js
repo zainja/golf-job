2edf09aeab1164731e7eb6045008087b453f7d13
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-07-02 22:12:00 +0100

    checking for admin in the auth

--- server/authHelpers/tokenAuth.js
+++ server/authHelpers/tokenAuth.js
@@ -2,23 +2,12 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken')
 module.exports.authToken = (req, res, next) =>{
     const authHeader = req.headers['authorization']
-    console.log(authHeader)
     const token = authHeader && authHeader.split(' ')[1]
     if (token == null) return res.sendStatus(401)
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, emailObj) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if (err) return res.sendStatus(403)
-        req.email = emailObj
-        next()
-    })
-}
-
-module.exports.registerTokenAuth = (req, res, next) => {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-    if (token == null) return res.status(401).send({msgs: "Confirmation email expired"})
-    jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, emailObj) => {
-        if (err) return res.status(403).send({msgs: "Confirmation email expired"})
-        req.email = emailObj
+        req.email = user.email
+        req.isAdmin = user.isAdmin
         next()
     })
 }
@@ -27,9 +16,9 @@ module.exports.resetPasswordTokenAuth = (req, res, next) => {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if (token == null) return res.status(401).send({msgs: "reset password link expired"})
-    jwt.verify(token, process.env.RESET_PASSWORD_TOKEN_SECRET, (err, emailObj) => {
+    jwt.verify(token, process.env.RESET_PASSWORD_TOKEN_SECRET, (err, user) => {
         if (err) return res.status(403).send({msgs: "reset password link expired"})
-        req.email = emailObj
+        req.email = user.email
         next()
     })
 }
@@ -38,9 +27,9 @@ module.exports.adminAccessToken = (req, res, next) => {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if (token == null) return res.status(401).send({msgs: "access denied"})
-    jwt.verify(token, process.env.ADMIN_TOKEN_SECRET, (err, emailObj) => {
+    jwt.verify(token, process.env.ADMIN_TOKEN_SECRET, (err, user) => {
         if (err) return res.status(403).send({msgs: "access denied"})
-        req.email = emailObj
+        req.email = user.email
         next()
     })
 
