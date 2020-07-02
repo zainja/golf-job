575531e9fbb58669bf21d50485b85ea020ede0bc
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-07-02 22:23:32 +0100

    fixing token errors

--- server/routes/user.js
+++ server/routes/user.js
@@ -18,8 +18,7 @@ router.post('/register', async (req, res) => {
     try{
         const hashedPassword = await hashPassword(password, 10)
         await user.register(email, firstName, lastName, hashedPassword, phoneNumber)
-        const emailObj = {email: email}
-        const registerToken = generateRegisterToken(emailObj)
+        const registerToken = generateRegisterToken(email)
         const random = crypto.randomBytes(64).toString('hex')
         await sendEmail(email, emailTemplate.confirm(random))
          res.json({
@@ -38,48 +37,43 @@ router.post('/login', async (req, res) => {
         const result = await user.login(email)
         if (result.length === 0){
             res.status(404)
-            res.send({error: "User Not found"})
+            res.send({msgs: "User Not found"})
         }
         else if (result[0].email_verified === false || result[0].email_verified === 0){
             res.status(403)
             const random = crypto.randomBytes(64).toString('hex')
             await sendEmail(email, emailTemplate.confirm(random))
-            const registerToken = generateRegisterToken({email: email})
+            const registerToken = generateRegisterToken(email)
             res.json({
-                error: emailMsgs.resend,
+                msgs: emailMsgs.resend,
                 registerToken: registerToken
             })
         }
         else if ( await comparePasswords(password, result[0].password)){
-            const emailObj = {
-                email: email
-            }
-            const accessToken = generateAccessToken(emailObj)
-            const refreshToken = generateRefreshToken(emailObj)
+            const accessToken = generateAccessToken(email, false)
             await res.json({
                 accessToken: accessToken,
-                refreshToken: refreshToken
             })
         }else {
             res.status(401)
-            res.send({error: "Incorrect email or password"})
+            res.send({msgs: "Incorrect email or password"})
         }
     }catch (e) {
         res.status(500)
-        res.send({"error": e})
+        res.send({msgs: e})
     }
 })
 
 // shared between user and admin
 router.put('/validate', tokenAuth.registerTokenAuth , async (req, res) => {
-    const {email} = req.email
+    const email = req.email
     try {
         await user.validateEmail(email)
         res.status(200)
-        res.send({success: "Verified"})
+        res.send({msgs: "Verified"})
     }catch (e) {
         res.status(401)
-        res.send({error: "verification failed"})
+        res.send({msgs: "verification failed"})
     }
 })
 
@@ -87,8 +81,7 @@ router.post('/request-reset-password', async (req, res) => {
     const {email} = req.body
     if (email === null) return res.status(404).send({msgs: "email must be filled"})
     try {
-        const emailObj = {email: email}
-        const resetPasswordToken = generateResetPasswordToken(emailObj)
+        const resetPasswordToken = generateResetPasswordToken(email)
         const random = crypto.randomBytes(64).toString('hex')
         await sendEmail(email, resetPasswordTemplate.reset(random))
         res.send({resetPasswordToken: resetPasswordToken})
@@ -99,7 +92,7 @@ router.post('/request-reset-password', async (req, res) => {
 })
 
 router.post('/reset-password', tokenAuth.resetPasswordTokenAuth, async (req, res) => {
-    const {email} = req.email
+    const email = req.email
     const {password} = req.body
     console.log(email)
     console.log(password)
