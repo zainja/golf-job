3e023118f71dcae8107e4d3db1e15463ff874a8b
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-07-02 13:41:58 +0100

    styling login and register

--- client/src/components/Forms/Login.js
+++ client/src/components/Forms/Login.js
@@ -1,5 +1,5 @@
 import {Link} from "react-router-dom";
-import React,{useState} from "react";
+import React, {useState} from "react";
 
 const Login = (props) => {
     const [email, setEmail] = useState("")
@@ -13,38 +13,45 @@ const Login = (props) => {
             <div className="container-fluid jumbotron text-center">
                 <h1 className="h1"> Login Page</h1>
             </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-2"/>
+                    <div className="col-md-8">
+                        <form onSubmit={onSubmit}>
+                            <div className="form-group">
+                                <label htmlFor="email">Email address</label>
+                                <input type="email"
+                                       className="form-control"
+                                       id="email"
+                                       value={email}
+                                       onChange={event => setEmail(event.target.value)}
+                                       aria-describedby="emailHelp"
+                                       required
+                                       placeholder="Enter email"/>
+                            </div>
 
-            <form className="container" onSubmit={onSubmit}>
-                <div className="form-group">
-                    <label htmlFor="email">Email address</label>
-                    <input type="email"
-                           className="form-control"
-                           id="email"
-                           value={email}
-                           onChange={event => setEmail(event.target.value)}
-                           aria-describedby="emailHelp"
-                           required
-                           placeholder="Enter email"/>
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <input
+                                    type="password"
+                                    value={password}
+                                    onChange={event => setPassword(event.target.value)}
+                                    className="form-control"
+                                    id="password"
+                                    required
+                                    placeholder="Password"
+                                />
+                            </div>
+                            <Link to="/forgot-password">
+                                <small> forgot your password ?</small>
+                            </Link>
+                            <br/>
+                            <button type="submit" className="btn btn-primary">Login</button>
+                        </form>
+                    </div>
+                    <div className="col-md-2"/>
                 </div>
-
-                <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={event => setPassword(event.target.value)}
-                        className="form-control"
-                        id="password"
-                        required
-                        placeholder="Password"
-                    />
-                </div>
-                <Link to="/forgot-password">
-                    <small> forgot your password ?</small>
-                </Link>
-                <br/>
-                <button type="submit" className="btn btn-primary">Login</button>
-            </form>
+            </div>
         </div>
     )
 }
--- client/src/components/Forms/Register.js
+++ client/src/components/Forms/Register.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import axios from "axios";
-import {useToasts } from 'react-toast-notifications'
+import {useToasts} from 'react-toast-notifications'
+
 const Register = (props) => {
     const {addToast} = useToasts()
     const [email, setEmail] = useState("")
@@ -10,92 +11,101 @@ const Register = (props) => {
     const [lastName, setLastName] = useState("")
     const [phone, setPhone] = useState("")
 
-    const onSubmit = (e) =>{
+    const onSubmit = (e) => {
         e.preventDefault()
-        if (password !== confirmPassword){
+        if (password !== confirmPassword) {
             addToast("passwords don't match", {appearance: "error", PlacementType: 'top-center'})
-        }else {
+        } else {
             props.onSubmit(email, firstName, lastName, phone, password)
         }
     }
-    return(
+    return (
         <div>
             <div className="container-fluid jumbotron text-center">
                 <h1 className="cm-header-2"> Register Page</h1>
             </div>
-            <form className="container" onSubmit={onSubmit}>
-                <div className="form-group">
-                    <h1 className="danger" id="errors"/>
-                    <label htmlFor="email">Email address</label>
-                    <input type="email"
-                           className="form-control"
-                           id="email"
-                           value={email}
-                           onChange={event => setEmail(event.target.value)}
-                           aria-describedby="emailHelp"
-                           placeholder="Enter email"
-                           required
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="firstName"> First Name</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="firstName"
-                        value={firstName}
-                        onChange={event => setFirstName(event.target.value)}
-                        placeholder="First Name"
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="lastName"> Last Name</label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        id="lastName"
-                        value={lastName}
-                        onChange={event => setLastName(event.target.value)}
-                        placeholder="Last Name"
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="phoneNumber"> Phone number </label>
-                    <input type="tel"
-                           className="form-control"
-                           id="phoneNumber"
-                           value={phone}
-                           onChange={event => setPhone(event.target.value)}
-                           placeholder="02932-2929323"
-                           required
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input type="password"
-                           className="form-control"
-                           id="password"
-                           value={password}
-                           onChange={event => setPassword(event.target.value)}
-                           required
-                           placeholder="Password"/>
-                </div>
 
-                <div className="form-group">
-                    <label htmlFor="confirmPassword">Confirm Password</label>
-                    <input type="password"
-                           className="form-control"
-                           id="confirmPassword"
-                           value={confirmPassword}
-                           onChange={event => setConfirmPassword(event.target.value)}
-                           required
-                           placeholder="Repeat Password"/>
-                   <small id="belowConfirm"/>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-2"/>
+                    <div className="col-md-8">
+                        <form onSubmit={onSubmit}>
+                            <div className="form-group">
+                                <h1 className="danger" id="errors"/>
+                                <label htmlFor="email">Email address</label>
+                                <input type="email"
+                                       className="form-control"
+                                       id="email"
+                                       value={email}
+                                       onChange={event => setEmail(event.target.value)}
+                                       aria-describedby="emailHelp"
+                                       placeholder="Enter email"
+                                       required
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="firstName"> First Name</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="firstName"
+                                    value={firstName}
+                                    onChange={event => setFirstName(event.target.value)}
+                                    placeholder="First Name"
+                                    required
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="lastName"> Last Name</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="lastName"
+                                    value={lastName}
+                                    onChange={event => setLastName(event.target.value)}
+                                    placeholder="Last Name"
+                                    required
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="phoneNumber"> Phone number </label>
+                                <input type="tel"
+                                       className="form-control"
+                                       id="phoneNumber"
+                                       value={phone}
+                                       onChange={event => setPhone(event.target.value)}
+                                       placeholder="02932-2929323"
+                                       required
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Password</label>
+                                <input type="password"
+                                       className="form-control"
+                                       id="password"
+                                       value={password}
+                                       onChange={event => setPassword(event.target.value)}
+                                       required
+                                       placeholder="Password"/>
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="confirmPassword">Confirm Password</label>
+                                <input type="password"
+                                       className="form-control"
+                                       id="confirmPassword"
+                                       value={confirmPassword}
+                                       onChange={event => setConfirmPassword(event.target.value)}
+                                       required
+                                       placeholder="Repeat Password"/>
+                                <small id="belowConfirm"/>
+                            </div>
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                    <div className="col-md-2"/>
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
+            </div>
         </div>
     )
 }
