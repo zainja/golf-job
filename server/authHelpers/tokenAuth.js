ba3c4b8c6e9f86d533a1cd6fa5864165cb0ad27a
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-07-02 10:33:25 +0100

    access token for admin fixed

--- server/authHelpers/tokenAuth.js
+++ server/authHelpers/tokenAuth.js
@@ -38,9 +38,9 @@ module.exports.adminAccessToken = (req, res, next) => {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if (token == null) return res.status(401).send({msgs: "access denied"})
-    jwt.verify(token, process.env.ADMIN_TOKEN_SECRET, (err, userObj) => {
-        if (err) return res.status(403).send({msgs: "access denided"})
-        req.userObj = userObj
+    jwt.verify(token, process.env.ADMIN_TOKEN_SECRET, (err, emailObj) => {
+        if (err) return res.status(403).send({msgs: "access denied"})
+        req.email = emailObj
         next()
     })
 
