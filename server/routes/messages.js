6df47aee21391966ff2c5ccf5d17437a331fb223
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-07-02 16:17:14 +0100

    message routes

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,13 +1,49 @@
 const express = require('express')
 const router = express.Router()
 const messagesQuery = require('../query/messagesQuery')
-router.post('/sendMessage', async (req, res) => {
-    const {subject, sender, receiver, message} = req.body
+const tokenAuth = require('../authHelpers/tokenAuth')
+router.post('/userSendMessage',tokenAuth.authToken, async (req, res) => {
+    const {email} = req.email
+    const {receiver, message} = req.body
     try{
-        await message(subject, sender, receiver, message)
-        res.send({msgs: "message sent"})
+        await messagesQuery.sendMessage(email, receiver, message)
+        res.send({msgs: "message send"})
     }catch (e) {
         res.send(e)
     }
 })
+
+router.post('/adminSendMessage', tokenAuth.adminAccessToken, async (req, res) => {
+    const {email} = req.email
+    const {receiver, message} = req.body
+    try{
+        await messagesQuery.sendMessage(email, receiver, message)
+        res.send({msgs: "message send"})
+    }catch (e) {
+        res.send(e)
+    }
+})
+
+router.post('/userGetMessages',tokenAuth.authToken, async (req, res) => {
+    const {email} = req.email
+    const {user} = req.body
+    try{
+        const messages = await messagesQuery.receiveAllMessages(email, user)
+        res.send({messages: messages})
+    }catch (e) {
+        res.send(e)
+    }
+})
+
+router.post('/adminGetMessages',tokenAuth.adminAccessToken, async (req, res) => {
+    const {email} = req.email
+    const {user} = req.body
+    try{
+        const messages = await messagesQuery.receiveAllMessages(email, user)
+        res.send({messages: messages})
+    }catch (e) {
+        res.send(e)
+    }
+})
+
 module.exports = router
