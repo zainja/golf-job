f42d5e47d1415334b20419cbedd6872738269bc3
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-07-06 23:19:40 +0100

    notes route

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -3,27 +3,61 @@ const express = require('express')
 const router = express.Router()
 const fs = require('fs')
 const path = require('path')
+const {getPostsForUser} = require("../query/notesQuery");
+const {getPostsFromAdmin} = require("../query/notesQuery");
+const {publishPost} = require("../query/notesQuery");
+const {authToken} = require("../authHelpers/tokenAuth");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null,path.resolve("publicfiles"))
+        cb(null, path.resolve("publicfiles"))
     },
     filename: function (req, file, cb) {
-        const name = file.originalname.replace(" ","-")
-        cb(null, Date.now()+ '-' + name)
+        cb(null, file.originalname)
     }
 })
 
-const upload = multer({ storage: storage }).single('file')
+const upload = multer({storage: storage}).single('file')
 
-router.post('/upload', (req, res) =>{
-    upload(req, res, (err) => {
-        if (err instanceof multer.MulterError){
-            return res.status(500).send(err)
-        }else if (err) {
-            return res.status(500).send(err)
+router.post('/upload', authToken, (req, res) => {
+    if (req.isAdmin) {
+        upload(req, res, (err) => {
+            if (err instanceof multer.MulterError) {
+                return res.status(500).send(err)
+            } else if (err) {
+                return res.status(500).send(err)
+            } else {
+                const {title, notes, targetClient} = req.body
+                const filePath =
+                    path.join(path.resolve("publicfiles"), req.file.originalname)
+                publishPost(req.email, targetClient, title, notes, filePath)
+                    .then(response => res.send(req.file))
+                    .catch(err => res.status(500).send({msgs: "error happened"}))
+            }
+        })
+    }
+    res.status(403).send({msgs: "Access Denied"})
+})
+
+router.get('/admin', authToken, async (req, res) => {
+    if (req.isAdmin) {
+        try {
+            const posts = await getPostsFromAdmin(req.email)
+            res.send({posts: posts})
+        } catch (e) {
+            res.send({msgs: "couldn't fetch posts"})
         }
-        return res.status(200).send(req.file)
-    })
+    }
+    res.status(403).send({msgs: "Access Denied"})
+})
+
+router.get('/user', authToken, async (req, res) => {
+    try {
+        const posts = await getPostsForUser(req.email)
+        res.send({posts: posts})
+    } catch (e) {
+        res.send({msgs: "couldn't fetch posts"})
+
+    }
 })
 module.exports = router
