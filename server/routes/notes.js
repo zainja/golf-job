5e42f45d8fc809699f529afb1029f26cc77e5da0
r60019zj <zain.jaffal@student.manchester.ac.uk>
2020-07-06 17:06:41 +0100

    uploading files to server

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,12 +1,16 @@
 const multer = require('multer')
 const express = require('express')
 const router = express.Router()
+const fs = require('fs')
+const path = require('path')
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'public')
+        cb(null,path.resolve("publicfiles"))
     },
     filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname)
+        const name = file.originalname.replace(" ","-")
+        cb(null, Date.now()+ '-' + name)
     }
 })
 
@@ -19,6 +23,8 @@ router.post('/upload', (req, res) =>{
         }else if (err) {
             return res.status(500).send(err)
         }
-        return res.status(200).send(req, file)
+        req.file
+        return res.status(200).send(req.file)
     })
 })
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const auth = require('./routes/user')
+const path = require("path")
 const admin = require('./routes/admin')
 const message = require('./routes/messages')
+const notes = require('./routes/notes')
 const port = 5000 | process.env.PORT
 const app = express();
 app.use(express.json());
@@ -15,4 +17,5 @@ app.get("/" , (req,res) => {
 app.use("/auth", auth )
 app.use("/admin", admin)
 app.use("/messages", message)
+app.use("/notes", notes)
 app.listen(port)
